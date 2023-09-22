377c93c13578295a20060646ba27e6e0f3223143
So0k4 <143562529+So0k4@users.noreply.github.com>
2023-09-22 17:07:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function login(element){
+    element.innerText = "Logout"
+    console.log("logout")
+}
+function remove(element){
+    element.remove();
+}
+function like(element){
+element.alert("Te gusta esta publicacion");
+}
